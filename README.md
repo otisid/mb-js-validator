e9c674bfbf57e987de43374f927393b2d004ab79
otisid <dandrase@gmail.com>
2015-11-04 14:16:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mbs-js-validator
+Simple JavaScript implementation of Croatian Business Registry Number (MBS) validaton.
+
+If the MBS is valid function returns 1, otherwise 0 (false).
+
+Example:
+
+> var MBS = '011302234';
+> validateMBS(MBS);
---
+++ mbsValidator.js
@@ -0,0 +1,56 @@
+// validates Croatian Business Registry Number (MBS)
+function validateMBS(mbsCode) {
+    // just in case
+    var code = mbsCode.toString();
+
+    var codeLength = code.length;
+    var res = '';
+
+    switch (codeLength) {
+        case 8:
+            break;
+        case 12:
+            code = code.substring(0, codeLength - 4);
+            codeLength = code.length;
+            break;
+        default:
+            return false;
+    }
+    
+    if (codeLength != 8 )
+        return false;
+
+    if (isNaN(code))
+        return false;
+
+    var sumz = '';
+    var count = codeLength - 1;
+    
+    for (var i = 0; i < count; i++) {
+        var fact = 8;
+        sumz = code.substr(i) + fact;
+        fact -= 1;
+    }
+    
+    var ost = sumz % 11;
+    var raz = 11 - ost;
+    var kont = code.charAt(codeLength - 1);
+
+    if (ost == 0) {
+        // not ok
+        res = 0;
+    } else if (ost == 1 && kont == 0) {
+        // ok
+        res = 1;
+    } else if (ost > 1 && ost < 11 && kont == raz) {
+        // ok
+        res = 1;
+    } else {
+        res = 0;
+    }
+
+    return res;
+}
+
+var test = 01130234;
+validateMBS(test);
